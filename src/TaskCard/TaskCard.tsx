4de9b3672f9ccfaf087184ca3ec2d00089f08c54
Antonio <antonioaugustosilvavaz@gmail.com>
2023-08-19 17:18:28 +0100

    cards get displayed

--- src/TaskCard/TaskCard.tsx
+++ src/TaskCard/TaskCard.tsx
@@ -3,12 +3,11 @@ import './TaskCard.css';
 
 export function TaskCard({ title, note, date, taskId, completed }: TaskType) {
   return (
-    <div className="task-card text-color">
-      <h3>title: {title}</h3>
-      <h3>note: {note}</h3>
-      <h3>date: {date}</h3>
-      <h3>taskId: {taskId}</h3>
-      <h3>completed: {completed}</h3>
+    <div className="task-card item-background-color item-text-color">
+      <h3 className='title'>{title}</h3>
+      <p>{note}</p>
+      <h4 className='date'>{date}</h4>
+      {/* <h3>completed: {completed}</h3> */}
     </div>
   )
 }
