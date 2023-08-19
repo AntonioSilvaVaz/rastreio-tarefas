6b4824edce57022bdfc1fea94df1e82872ee6317
Antonio <antonioaugustosilvavaz@gmail.com>
2023-08-19 15:34:48 +0100

    create task menu completed

--- src/CreateTask/CreateTask.tsx
+++ src/CreateTask/CreateTask.tsx
@@ -13,34 +13,29 @@ export function CreateTask() {
       {
         showCreateTask &&
         <>
-          <div className='softBackground background-color'></div>
+          <div onClick={() => setShowCreateTask(false)} className='softBackground background-color'></div>
           <div className='create-task item-text-color item-background-color'>
-
             <div className='title-btn-container'>
-              <h2>Create a new task</h2>
-              <button onClick={() => setShowCreateTask(false)} className='close-btn pointer'>X</button>
+              <h2>Cria um nova tarefa</h2>
+              <button onClick={() => setShowCreateTask(false)} className='close-btn pointer item-text-color'>X</button>
             </div>
-
             <div className='input-container'>
               <div>
                 <label htmlFor="title"><h4>Title</h4></label>
-                <input type="text" name="title" className='item-text-color' />
+                <input type="text" name="title" />
               </div>
-
               <div>
                 <label htmlFor="note"><h4>Nota</h4></label>
                 <textarea className='textarea' name="note"></textarea>
               </div>
-
               <div>
                 <label htmlFor="date"><h4>Data</h4></label>
-                <input type="datetime-local" name='date' className='item-text-color' />
+                <input type="datetime-local" name='date' />
               </div>
+
+              <button className='background-color text-color' type="submit">Criar tarefa</button>
             </div>
-
-
           </div>
-
         </>
       }
 
