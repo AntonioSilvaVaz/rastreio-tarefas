0aef41ebebcf8676acf70f2620ecb73299c61959
Antonio <antonioaugustosilvavaz@gmail.com>
2023-08-22 16:48:34 +0100

    changed back minTime

--- src/utilities/date.tsx
+++ src/utilities/date.tsx
@@ -1,10 +1,7 @@
 export function prettifyDate(date: string): string {
-
   let [dayMonthYear, time] = date.split('T');
-
   let [year, month, day] = dayMonthYear.split('-');
   let [hour, minutes] = time.split(':');
-
   return `Limite: ${day}-${month}-${year} até ${hour}:${minutes}`;
 }
 
@@ -13,5 +10,7 @@ export function getDatetimeLocalNow(): string {
   const day = dateTime.getDate();
   const month = dateTime.getMonth() + 1;
   const year = dateTime.getFullYear();
-  return `${year}-${month > 9 ? month : '0' + month}-${day > 9 ? day : '0' + day}T00:00`;
+  const hour = dateTime.getHours();
+  const minutes = dateTime.getMinutes() + 1;
+  return `${year}-${month > 9 ? month : '0' + month}-${day > 9 ? day : '0' + day}T${hour > 9 ? hour : '0' + hour}:${minutes > 9 ? minutes : '0' + minutes}`;
 }
